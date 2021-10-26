80caab5f98b725b0895d95c7c5b6a2e526b6a71a
xunwei <yeexunwei@gmail.com>
2021-10-27 02:19:49 +0800

    Add split_dataset method

--- scraper/preprocess.py
+++ scraper/preprocess.py
@@ -2,6 +2,7 @@ import os
 from joblib import Parallel, delayed
 from os.path import join
 from pathlib import Path
+import random
 import shutil
 
 import scraper
@@ -17,18 +18,24 @@ logger = logging.getLogger()
 # logging.basicConfig(level="INFO", format="%(levelname)s: %(filename)s: %(message)s")
 # logging.basicConfig(level='WARNING', format="%(levelname)s: %(filename)s: %(message)s")
 # logger.setLevel(logging.WARNING)
-logger.disabled = True
 
+
+random.seed(42)
 AUDIO_EXTENSION = 'mp3'
-READ_FILE_THREADS = 1
-path = '../data/TedSrt'
-split = 'train'
+READ_FILE_THREADS = 4
+corpus_path = '../data/TedSrt'
+processed_data_path = join(corpus_path, 'processed_data') # store all data in this folder before splitting
 
 src_path = 'data'
 
-
+# filter audio and srt
 DURATION_DIFF = 7 # seconds
 REPEATED_OCCURRENCE = 50
+
+# train test split
+ratio=[.6, .2, .2]
+
+
 
 def clean_text(text):
     '''
@@ -102,7 +109,7 @@ def txt_to_trans(txt_file, file_name):
 
 def save_txt(txt, output_path):
     '''
-    save transcript to output_path
+    Save transcript to output_path
     '''
     if not os.path.exists(os.path.dirname(output_path)):
         os.makedirs(os.path.dirname(output_path))
@@ -113,6 +120,9 @@ def save_txt(txt, output_path):
         f.close()
 
 def check_ms_accuracy(time_slices, occurrence_threshold=20):
+    '''
+    Check whether srt is accurate, remove those not accurate to milliseconds, check repeated occurrence of 820
+    '''
     for time_slice in time_slices:
         occurrence_820 = sum(1 for elem in time_slices if elem[1][0] % 1000 == 820)
     if occurrence_820 > REPEATED_OCCURRENCE:
@@ -120,43 +130,52 @@ def check_ms_accuracy(time_slices, occurrence_threshold=20):
     return 0
 
 def check_intro_timing(audio_duration, audio_srt_duration):
+    '''
+    Check whether srt matches audio, remove those without taking water drop intro into account
+    '''
     duration_diff = audio_duration - audio_srt_duration
     if duration_diff > DURATION_DIFF:
         return duration_diff
     return 0
 
-def convert(src_path=src_path):
-    shutil.rmtree(join(path, split), ignore_errors=True)
-    folder_list = os.listdir(src_path)
-    for idx, curr_folder in enumerate(tqdm(folder_list, desc="Looping over folders")):
-        logging.info('\n')
+def build_dataset(src_path=src_path):
+    '''
+    Build dataset from raw data scraper/data/
+    split audio into slices
+    convert srt to trans.txt ready for training
+    '''
+    # logger.disabled = True
+    shutil.rmtree(corpus_path, ignore_errors=True)
+    folders = os.listdir(src_path)
+    for idx, curr_folder in enumerate(tqdm(folders, desc="Building dataset from scraped data")):
+        # logging.info('\n')
         file_name = str(idx) #save the transcript as num, can be changed to folder name
-        output_path = join(path, split, file_name)
+        output_path = join(processed_data_path, file_name)
         txt_output_path = join(output_path, file_name + '.trans.txt')
         txt_src_path = 'tedsrt-lm-norm.txt'
-        txt_src_path = join(path, split, txt_src_path)
+        txt_src_path = join(corpus_path, txt_src_path)
                 
-        logging.info(f"{idx}. Creating transcript for {curr_folder}...")
+        # logging.info(f"{idx}. Creating transcript for {curr_folder}...")
         txt_path = list(Path(join(src_path, curr_folder)).rglob('*.txt'))[0]
         transcript, time_slices, txt_src = txt_to_trans(txt_path, file_name)
         
-        logging.info(f"{idx}. Slicing audio for {curr_folder}...")
+        # logging.info(f"{idx}. Slicing audio for {curr_folder}...")
         audio_path = list(Path(join(src_path, curr_folder)).rglob('*.' + AUDIO_EXTENSION))[0]
         audio_file = AudioSegment.from_file(audio_path, AUDIO_EXTENSION)
         
-        # check whether srt is accurate, remove those not accurate to milliseconds, check repeated occurrence of 820
+        # check timing accuracy by milliseconds
         ms_not_accurate = check_ms_accuracy(time_slices)
         if ms_not_accurate:
-            logging.warning(f"{idx}. Srt not accurate with {ms_not_accurate} 820s. Deleting entry {curr_folder}")
+            # logging.warning(f"{idx}. Srt not accurate with {ms_not_accurate} 820s. Deleting entry {curr_folder}")
             shutil.rmtree(output_path, ignore_errors=True)
             continue
         
-        # check whether srt matches audio, remove those without taking water drop intro into account
+        # check timing of audio intro
         audio_duration = audio_file.duration_seconds
         audio_srt_duration = time_slices[-1][-1][-1] / 1000
         intro_not_matched = check_intro_timing(audio_duration, audio_srt_duration)
         if intro_not_matched:
-            logging.warning(f"{idx}. Srt not matching with time slices. Deleting entry {curr_folder}")
+            # logging.warning(f"{idx}. Srt not matching with time slices. Deleting entry {curr_folder}")
             shutil.rmtree(output_path, ignore_errors=True)
             continue
         
@@ -171,13 +190,38 @@ def convert(src_path=src_path):
         tqdm.write(f'Successfully created {idx} {curr_folder}')
         # print(f'Successfully created {idx} {curr_folder}')
 
-def main():
-    print('Scraping data')
-    scraper.main(number_of_talks=200, starting_video_id=100)
-    print()
-    print('Start preprocessing')
-    convert(src_path)
+def move_folders(folders, source, destination):
+    '''
+    Move a list of folders from source to destination
+    '''
+    logger.info(f'{destination.split("/")[-1]}: {folders}')
+    for folder in folders:
+        shutil.move(join(source, folder), join(destination, folder))
 
+def split_dataset(ratio=[.6, .2, .2]):
+    '''
+    Split dataset to train dev test according to ratio
+    '''
+    folders = os.listdir(processed_data_path)
+    folders.sort()
+    random.shuffle(folders)
+
+    split_1 = int(ratio[0] * len(folders))
+    split_2 = int((ratio[0] + ratio[1]) * len(folders))
+
+    move_folders(folders[:split_1], processed_data_path, join(corpus_path, 'train'))
+    move_folders(folders[split_1:split_2], processed_data_path, join(corpus_path, 'dev'))
+    move_folders(folders[split_2:], processed_data_path, join(corpus_path, 'test'))
+    shutil.rmtree(processed_data_path, ignore_errors=True)
+
+
+def main():
+    print('Scraping data...')
+    # scraper.main(number_of_talks=200, starting_video_id=100)
+    print()
+    print('Start preprocessing...')
+    build_dataset(src_path)
+    split_dataset(ratio=ratio)
 
 
 if __name__ == "__main__":
