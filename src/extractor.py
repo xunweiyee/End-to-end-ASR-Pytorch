5f80dabfcb9d2bd6f50ace702828fa219dfad269
xunwei <yeexunwei@gmail.com>
2021-11-11 01:31:53 +0800

    Complete rnnextractor includes downsampling

--- src/extractor.py
+++ src/extractor.py
@@ -1,6 +1,12 @@
 import torch
 from torch import nn as nn
 
+import logging
+logger = logging.getLogger()
+logging.basicConfig(level="INFO", format="%(filename)s: %(message)s")
+# logging.basicConfig(level="INFO", format="%(levelname)s: %(filename)s: %(message)s")
+logger.disabled = True
+
 
 class VGGExtractor(nn.Module):
     ''' VGG extractor for ASR described in https://arxiv.org/pdf/1706.02737.pdf'''
@@ -13,6 +19,7 @@ class VGGExtractor(nn.Module):
         self.in_channel = in_channel
         self.freq_dim = freq_dim
         self.out_dim = out_dim
+        logging.info(f"VGGExtractor: input_dim {input_dim}, in_channel {self.in_channel}, freq_dim {self.freq_dim}, out_dim {self.out_dim}")
 
         self.extractor = nn.Sequential(
             nn.Conv2d(in_channel, self.init_dim, 3, stride=1, padding=1),
@@ -54,14 +61,25 @@ class VGGExtractor(nn.Module):
         return feature, feat_len
 
     def forward(self, feature, feat_len):
+
+        # Feature shape BSxTxD -> BS x CH(num of delta) x T x D(acoustic feature dim)
+        # BSx128xT/4xD/4 -> BSxT/4x128xD/4
+        #  BS x T/4 x 128 x D/4 -> BS x T/4 x 32D
+
+        logging.info(f"VGGExtractor: feature, feat_len {feature.shape}, {feat_len}")
         # Feature shape BSxTxD -> BS x CH(num of delta) x T x D(acoustic feature dim)
         feature, feat_len = self.view_input(feature, feat_len) #downsample on time
+        logging.info(f"VGGExtractor: downsampled to {feature.shape}, {feat_len}")
         # Foward
         feature = self.extractor(feature)
+        logging.info(f"VGGExtractor: extracted {feature.shape}")
         # BSx128xT/4xD/4 -> BSxT/4x128xD/4
         feature = feature.transpose(1, 2)
+        logging.info(f"VGGExtractor: feature.transpose {feature.shape}")
         #  BS x T/4 x 128 x D/4 -> BS x T/4 x 32D
         feature = feature.contiguous().view(feature.shape[0], feature.shape[1], self.out_dim)
+        logging.info(f"VGGExtractor: feature transformed {feature.shape}\n")
+
         return feature, feat_len
 
 
@@ -74,18 +92,91 @@ class MLPExtractor(nn.Module):
     def __init__(self, input_dim, out_dim):
         super(MLPExtractor, self).__init__()
 
+        logging.info(f"MLPExtractor: {input_dim}")
         self.out_dim = out_dim
-        self.hid_dim = input_dim * 3
+        self.hide_dim = input_dim * 3
         self.extractor = nn.Sequential(
-            nn.Linear(input_dim, self.hid_dim),
+            nn.Linear(input_dim, self.hide_dim),
             nn.ReLU(),
-            nn.Linear(self.hid_dim, self.hid_dim),
+            nn.Linear(self.hide_dim, self.hide_dim),
             nn.ReLU(),
-            nn.Linear(self.hid_dim, self.hid_dim),
+            nn.Linear(self.hide_dim, self.hide_dim),
             nn.ReLU(),
-            nn.Linear(self.hid_dim, self.hid_dim),
+            nn.Linear(self.hide_dim, self.hide_dim),
             nn.ReLU(),
-            nn.Linear(self.hid_dim, self.out_dim),
+            nn.Linear(self.hide_dim, self.out_dim),
+        )
+
+    def reshape_input(self, feature, group_size):
+        down_sample_len = feature.size(1) // group_size
+        feature = feature[:,:down_sample_len*group_size,:]
+        reshape_feature = feature.reshape(feature.size(0) * down_sample_len, group_size*feature.size(2))
+        return reshape_feature
+
+
+    def forward(self, feature, feat_len):
+        bs = feature.size(0)
+        feature = self.reshape_input(feature, group_size=4)
+        raw_output = self.extractor(feature)
+
+        reshape_output = raw_output.reshape(bs, raw_output.size(0) // bs, self.out_dim)
+        return reshape_output, torch.div(feat_len,4, rounding_mode="floor")
+
+
+class RNNExtractor(nn.Module):
+    ''' A simple 2-layer RNN extractor for acoustic feature down-sampling'''
+
+    def __init__(self, input_dim, out_dim):
+        super(RNNExtractor, self).__init__()
+
+        self.out_dim = out_dim
+        self.layer = nn.RNN(input_dim, self.out_dim)
+        # self.ln = nn.LayerNorm(self.out_dim)
+        self.dp = nn.Dropout(p=0.2)
+        self.linear = nn.Linear(self.out_dim, self.out_dim)
+
+    def forward(self, feature, feat_len):
+
+        logging.info(f"RNNExtractor: forward {feature.shape}, {feat_len}")
+        # feat_len = feat_len//4
+        feat_len = torch.div(feat_len, 4, rounding_mode='floor')
+        feature, _ = self.layer(feature)
+        # Normalization
+        feature = self.dp(feature)
+        logging.info(f"RNNExtractor: extracted {feature.shape}")
+        
+        # Downsample timestep
+        sample_rate = 4
+        if feature.shape[1] % sample_rate != 0:
+            feature = feature[:, :-(feature.shape[1] % 4), :].contiguous()
+        feature = feature[:, ::sample_rate, :].contiguous()
+        logging.info(f"RNNExtractor: downsampled {feature.shape} {feat_len}\n")
+
+        return feature, feat_len
+
+
+
+class ANNExtractor(nn.Module):
+    '''
+        A simple MLP extractor for acoustic feature down-sampling
+        Every 4 frame will be convert to corresponding features by MLP
+    '''
+
+    def __init__(self, input_dim, out_dim):
+        super(ANNExtractor, self).__init__()
+
+        self.out_dim = out_dim
+        self.hide_dim = input_dim * 3
+        self.extractor = nn.Sequential(
+            nn.Linear(input_dim, self.hide_dim),
+            nn.ReLU(),
+            nn.Linear(self.hide_dim, self.hide_dim),
+            nn.ReLU(),
+            nn.Linear(self.hide_dim, self.hide_dim),
+            nn.ReLU(),
+            nn.Linear(self.hide_dim, self.hide_dim),
+            nn.ReLU(),
+            nn.Linear(self.hide_dim, self.out_dim),
         )
 
     def reshape_input(self, feature, group_size):
