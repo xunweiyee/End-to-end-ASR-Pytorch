8fd519f969f72ac0d068af3b9cfba3d8be3ab782
Sam-limyr <nacuniel@gmail.com>
2021-11-11 21:01:12 +0800

    feat: Implement CNNClassifier.

--- src/classifier.py
+++ src/classifier.py
@@ -30,3 +30,39 @@ class MLPCLassfier(nn.Module):
 
 
         return self.classifier(feature)
+
+
+class CNNClassifier(nn.Module):
+    def __init__(self, input_dim):
+        super(CNNClassifier, self).__init__()
+        hidden_dim_1 = 64
+        hidden_dim_2 = hidden_dim_1 * 2
+
+        self.out_dim = input_dim * 8
+        self.classifier = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=hidden_dim_1, kernel_size=(3,), stride=(1,), padding=(1,)),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_dim_1, out_channels=hidden_dim_1, kernel_size=(3,), stride=(1,), padding=(1,)),
+            nn.ReLU(),
+            nn.MaxPool2d(2, stride=2),
+            nn.Conv2d(in_channels=hidden_dim_1, out_channels=hidden_dim_2, kernel_size=(3,), stride=(1,), padding=(1,)),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_dim_2, out_channels=hidden_dim_2, kernel_size=(3,), stride=(1,), padding=(1,)),
+            nn.ReLU(),
+            nn.MaxPool2d(2, stride=2),
+            nn.Linear((input_dim // 4) * hidden_dim_2, self.out_dim),
+        )
+
+
+    def reshape_input(self, feature, group_size):
+        down_sample_len = feature.size(1) // group_size
+        feature = feature[:,:down_sample_len*group_size,:]
+        reshape_feature = feature.reshape(feature.size(0) * down_sample_len, group_size*feature.size(2))
+        return reshape_feature
+
+
+    def forward(self, feature):
+        print(f"{feature}\n\nSize:{feature.size()}")
+        return self.classifier(feature)
+
+
